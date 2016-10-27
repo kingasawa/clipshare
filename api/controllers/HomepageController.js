07270f71672798dcb3cedc22bdf1e0ef34d5bd4a
King <kingasawa@gmail.com>
2016-10-27 19:22:32 +0700

    edit home page

--- api/controllers/HomepageController.js
+++ api/controllers/HomepageController.js
@@ -1,17 +1,34 @@
 /**
  * HomepageController
- *
+ * Author       :: Kingasawa
  * @description :: Server-side logic for managing homepages
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ * @help        :: https://facebook.com/kingasawa.chan
  */
 
 module.exports = {
-  index: (req,res) => {
-    // let sampleDate = (new Date()).toString();
 
-    Post.find(function(err,foundPost) {
-      return res.view("homepage",{foundPost})
+  index: (req,res) => {
+    let presentDate = (new Date()).toString();
+
+    let findFeaturedPost = new Promise((resolve, reject) => {
+      Post.find({featured:1}).exec((err, featuredPost) => {
+        if (err) {reject(err)}
+        resolve(featuredPost);
+      })
     });
+    let findAllCategory = new Promise((resolve, reject) => {
+      Category.find().populate('posts').exec((err, allCategory) => {
+        if (err) {reject(err)}
+        resolve(allCategory);
+      })
+    });
+    (async () => {
+      var [featuredPost,allCategory] = await Promise.all([findFeaturedPost,findAllCategory]);
+      console.log(allCategory);
+      return res.view("homepage", {featuredPost,allCategory})
+    })
+    ()
   }
+
 };
 
