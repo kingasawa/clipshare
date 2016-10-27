bfd1a0e353f9475968c3bb71fbe273f2d59b3c70
King <kingasawa@gmail.com>
2016-10-27 21:49:36 +0700

    edit home page

--- api/controllers/HomepageController.js
+++ api/controllers/HomepageController.js
@@ -9,23 +9,64 @@ module.exports = {
 
   index: (req,res) => {
     let presentDate = (new Date()).toString();
+    let postLimit = 32;
+    let params = req.allParams();
 
+    // Count all post
+    let findCountPost = new Promise((resolve, reject) => {
+      Post.count().exec((err, countPost) => {
+        if (err) {reject(err)}
+        resolve(countPost);
+      })
+    });
+    // Select all Featured Post - no limit
     let findFeaturedPost = new Promise((resolve, reject) => {
       Post.find({featured:1}).exec((err, featuredPost) => {
         if (err) {reject(err)}
         resolve(featuredPost);
       })
     });
+    // Select all Category - no limit
     let findAllCategory = new Promise((resolve, reject) => {
       Category.find().populate('posts').exec((err, allCategory) => {
         if (err) {reject(err)}
         resolve(allCategory);
       })
     });
+    // Select all Post - limit 32
+    let findAllPost = new Promise((resolve, reject) => {
+      if (!params.page) {
+        Post.find().limit(postLimit).exec((err, allPost) => {
+          if (err) {
+            reject(err)
+          }
+          resolve(allPost);
+        })
+      } else {
+        Post.find().paginate({params,limit:postLimit}).exec((err, allPost) => {
+          if (err) {
+            reject(err)
+          }
+          resolve(allPost);
+        })
+      }
+      });
+    // } else {
+    //   let findAllPost = new Promise((resolve, reject) => {
+    //     Post.find().paginate({page:params.page,limit:postLimit}).exec((err, allPost) => {
+    //       if (err) {reject(err)}
+    //       resolve(allPost);
+    //     })
+    //   });
+    // }
+    // Solve all using Async/Await
     (async () => {
-      var [featuredPost,allCategory] = await Promise.all([findFeaturedPost,findAllCategory]);
-      console.log(allCategory);
-      return res.view("homepage", {featuredPost,allCategory})
+      var [featuredPost,allCategory,allPost] = await Promise.all([
+        findFeaturedPost,
+        findAllCategory,
+        findAllPost
+      ]);
+      return res.view("homepage", {featuredPost,allCategory,allPost})
     })
     ()
   }
